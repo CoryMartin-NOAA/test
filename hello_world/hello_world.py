8916d36839eff6246b95daedba0268c4a0418870
Joshua Kumm <jkumm@Orion-login-4.HPC.MsState.Edu>
2022-06-15 14:24:36 -0500

    Changed name from "cory" to "josh" in python script

--- hello_world/hello_world.py
+++ hello_world/hello_world.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 # define some strings
-myname = "Cory Martin"
-myemail = "cory.r.martin@noaa.gov"
+myname = "Joshua Kumm"
+myemail = "joshua.kumm@noaa.gov"
 
 # print statement
 print(f"Hello world! My name is {myname} and my email address is {myemail}.")
