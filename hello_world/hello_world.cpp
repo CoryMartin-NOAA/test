af476e3a8fefdc22a78220605fde02d681acb7cb
Cory Martin <cory.r.martin@noaa.gov>
2022-06-15 11:50:52 -0500

    Add sample hello world files

---
+++ hello_world/hello_world.cpp
@@ -0,0 +1,10 @@
+// C++ is even more complicated than Fortran
+#include <iostream> // this is to get the std::cout
+#include <string> // this is to get the string type
+
+int main() {
+  std::string myname = "Cory Martin";
+  std::string myemail = "cory.r.martin@noaa.gov";
+  std::cout << "Hello world! My name is " << myname << " and my email address is "
+  << myemail << "." << std::endl;
+} // C++ loves brackets...
